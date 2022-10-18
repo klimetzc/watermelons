8fd22ff5bcaf971eebe50fd0de6cb8f825b523e9
Айдар Шарафутдинов <shaidarharan87@gmail.com>
2022-10-19 00:17:35 +0300

    feat: add landing component

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -5,6 +5,29 @@ import WatermelonSlice from '../../shared/ui/WatermelonSlice/WatermelonSlice';
 import './Landing.scss';
 
 const Landing = () => {
+  interface IFeature {
+    name: string;
+    desc: string;
+    img: string;
+  }
+  const featuresHard: IFeature[] = [
+    {
+      name: 'Наша миссия',
+      desc: 'Обеспечить безопасные и выгодные сделки между продавцом и покупателем',
+      img: './static/images/feature-1.webp',
+    },
+    {
+      name: 'Совместные закупки',
+      desc: 'Объединившись, покупатели смогут выкупить товары по низкой цене со скидкой, а продавцы - реализуют сразу большие партии товаров',
+      img: './static/images/feature-2.webp',
+    },
+    {
+      name: 'Проверка товара до покупки',
+      desc: 'Покупатель может удостовериться в подлинности товара, проверив серийный номер',
+      img: './static/images/feature-3.webp',
+    },
+  ];
+
   useEffect(() => {
     document.title = 'Watermelons';
   }, []);
@@ -27,6 +50,23 @@ const Landing = () => {
       <p className="landing-page__slogan">
         Мы делаем любимые вещи доступнее для каждого
       </p>
+      <div className="landing-page__features">
+        {featuresHard.map((feature) => (
+          <div className="feature" key={feature.name}>
+            <div className="feature__description">
+              <h3>{feature.name}</h3>
+              <p>{feature.desc}</p>
+            </div>
+            <div className="feature__img">
+              <img src={feature.img} alt="" />
+            </div>
+          </div>
+        ))}
+      </div>
+      <Link to="/categories">
+        <ButtonMelon size="large">Перейти к покупкам</ButtonMelon>
+      </Link>
+      <footer>Тут будет футер</footer>
     </div>
   );
 };
