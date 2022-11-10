0c395b4290b90f0ab8fabe6978b313a7e22507f4
Anton Belko <30528165+ko1p@users.noreply.github.com>
2022-11-10 12:48:16 +0300

    fix lint warning

--- src/pages/SellerDashboard/layout/SellerOrders/SellerOrders.tsx
+++ src/pages/SellerDashboard/layout/SellerOrders/SellerOrders.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { ISellerOrder } from 'shared/api/types/interfaces';
 import './SellerOrders.scss';
@@ -10,28 +9,24 @@ interface IProps {
   label: string;
 }
 
-const SellerOrders: React.FC<IProps> = ({ orders, emptyMessage, label }) => {
-  const { t } = useTranslation();
-
-  return (
-    <div className="seller-dashboard__orders">
-      <h3 className="seller-dashboard__orders-title">{label}:</h3>
-      {orders?.length ? (
-        orders.map((item) => (
-          <Link
-            to={`/dashboard/orders/${item.id}`}
-            className="seller-dashboard__order-card"
-            key={item.id}
-          >
-            <p>{item.id}</p>
-            <p>{item.orderStatus}</p>
-            <p>{item.clientName}</p>
-          </Link>
-        ))
-      ) : (
-        <p className="seller-dashboard__orders-empty">{emptyMessage}</p>
-      )}
-    </div>
-  );
-};
+const SellerOrders: React.FC<IProps> = ({ orders, emptyMessage, label }) => (
+  <div className="seller-dashboard__orders">
+    <h3 className="seller-dashboard__orders-title">{label}:</h3>
+    {orders?.length ? (
+      orders.map((item) => (
+        <Link
+          to={`/dashboard/orders/${item.id}`}
+          className="seller-dashboard__order-card"
+          key={item.id}
+        >
+          <p>{item.id}</p>
+          <p>{item.orderStatus}</p>
+          <p>{item.clientName}</p>
+        </Link>
+      ))
+    ) : (
+      <p className="seller-dashboard__orders-empty">{emptyMessage}</p>
+    )}
+  </div>
+);
 export default SellerOrders;
