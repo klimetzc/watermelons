6035cb2af6af73d1330aa16a0668f854a744e938
Klim Orlov <flameko-dan@yandex.ru>
2022-11-12 18:10:05 +0300

    refactor: product page decomposition

--- src/pages/productPage/ProductPage.tsx
+++ src/pages/productPage/ProductPage.tsx
@@ -1,22 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React, { useEffect, useState } from 'react';
-import {
-  HomeOutlined,
-  InfoCircleOutlined,
-  LikeOutlined,
-} from '@ant-design/icons';
-import {
-  Breadcrumb,
-  Col,
-  message,
-  Progress,
-  Rate,
-  Row,
-  Skeleton,
-  Statistic,
-  Tooltip,
-  Typography,
-} from 'antd';
+import { HomeOutlined } from '@ant-design/icons';
+import { Breadcrumb, Rate, Skeleton } from 'antd';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import BuyBucketButton from 'features/client/buy-bucket-btn/ui/BuyBucketButton';
@@ -24,19 +9,11 @@ import './ProductPage.scss';
 import { dom, utils } from 'shared/lib';
 import { categoriesEndpoints } from 'shared/api/categories.endpoints';
 import { useTranslation } from 'react-i18next';
-import ButtonMelon from 'shared/ui/ButtonMelon/ButtonMelon';
-import moment from 'moment';
-import { preordersEndpoints } from 'shared/api/preorders.enpoints';
-import { useSelector } from 'react-redux';
-import { RootState } from 'app/store';
+import { CollabWidget } from './layout/CollabWidget';
 
 const ProductPage: React.FC = () => {
   // TODO: Декомпозировать подписку в фичу
   const params = useParams();
-  const [preorderParticipate] =
-    preordersEndpoints.useParticipatePreorderMutation();
-  const [isUserSubscribed, setIsUserSubscribed] = useState<boolean>(false);
-
   dom.useTitle(`Товар № ${params.productId}`);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { data: category, isLoading: isCategoryLoading } =
@@ -48,27 +25,9 @@ const ProductPage: React.FC = () => {
     });
   const { t } = useTranslation();
 
-  const isClientLogged = useSelector(
-    (state: RootState) => state.userAuthReducer.isLoggedIn
-  );
-  const isSellerLogged = useSelector(
-    (state: RootState) => state.sellerAuthReducer.isLoggedIn
-  );
-
   useEffect(() => {
     setIsLoading(isCategoryLoading && isProductDataLoading);
   }, [isCategoryLoading, isProductDataLoading]);
-
-  const handlePreorderParticipate = async () => {
-    try {
-      await preorderParticipate(params.productId!).unwrap();
-      message.success('Вы подписались на предзаказ');
-      setIsUserSubscribed(true);
-    } catch (err) {
-      console.log(err);
-      message.error('При оформлении предзаказа произошла ошибка');
-    }
-  };
 
   return (
     <div className="product-page">
@@ -89,109 +48,7 @@ const ProductPage: React.FC = () => {
         </Breadcrumb>
       </div>
       {productData?.preorder ? (
-        <div className="product-page__collab-widget">
-          <div className="product-page__collab-info">
-            <InfoCircleOutlined style={{ fontSize: '28px' }} />
-            <Typography.Paragraph className="product-page__collab-info-paragraph">
-              Для {productData?.title || 'имя'} доступны совместные закупки!
-              Можно предзаказать товар с хорошей скидкой и как только соберётся
-              нужное кол-во покупателей - товар помчится к вам в руки!
-            </Typography.Paragraph>
-          </div>
-          <Row gutter={8} className="product-page__collab-progress">
-            <Col lg={12} md={24} className="product-page__collab-progress-bars">
-              <Row justify="space-between" gutter={8}>
-                <Col lg={8} md={16}>
-                  <Progress
-                    type="circle"
-                    strokeColor={{
-                      '0%': '#108ee9',
-                      '100%': '#87d068',
-                    }}
-                    percent={
-                      productData
-                        ? utils.getPercentFromValue(
-                            isUserSubscribed
-                              ? +productData!.preorder!
-                                  .preorderCurrentQuantity + 1
-                              : +productData!.preorder!.preorderCurrentQuantity,
-                            +productData!.preorder!.preorderExpectedQuantity
-                          )
-                        : 0
-                    }
-                  />
-                </Col>
-                <Col lg={16} md={8}>
-                  <div className="product-page__collab-progress-info">
-                    <div className="product-page__collab-body-counter">
-                      {isUserSubscribed
-                        ? +productData!.preorder!.preorderCurrentQuantity + 1
-                        : productData?.preorder?.preorderCurrentQuantity}{' '}
-                      из {productData?.preorder?.preorderExpectedQuantity}
-                    </div>
-                    <div className="product-page__collab-date-counter">
-                      до{' '}
-                      {moment(productData?.preorder?.preorderEndsAt).format(
-                        'DD.MM.YYYY'
-                      )}
-                    </div>
-                    <div className="product-page__collab-days-counter">
-                      осталось{' '}
-                      {moment(productData?.preorder?.preorderEndsAt).diff(
-                        moment(new Date()),
-                        'days'
-                      )}{' '}
-                      дней
-                    </div>
-                  </div>
-                </Col>
-              </Row>
-            </Col>
-            <Col lg={12} md={24}>
-              <div className="product-page__statistics">
-                <div>
-                  <Statistic.Countdown
-                    value={moment(
-                      productData?.preorder?.preorderEndsAt
-                    ).format()}
-                    format="D дней HH часов mm минут"
-                  />
-                </div>
-                <div className="product-page__collab-feedback">
-                  <Statistic
-                    title="Оценок"
-                    value={1128}
-                    prefix={<LikeOutlined />}
-                  />
-                </div>
-              </div>
-            </Col>
-          </Row>
-          {!isSellerLogged && (
-            <div className="product-page__collab-actions">
-              {isClientLogged ? (
-                <ButtonMelon
-                  size="large"
-                  type="primary"
-                  onClick={handlePreorderParticipate}
-                >
-                  Предзаказать
-                </ButtonMelon>
-              ) : (
-                <Tooltip title="Нужно сначала авторизоваться">
-                  <Typography.Paragraph disabled>
-                    Предзаказ доступен только после регистрации
-                  </Typography.Paragraph>
-                </Tooltip>
-              )}
-
-              <p className="product-page__collab-price">
-                {productData?.price || 0}{' '}
-                {utils.getCurrencyString(`${productData?.currency}`) || '$'}
-              </p>
-            </div>
-          )}
-        </div>
+        <CollabWidget productData={productData} />
       ) : null}
 
       <div className="product-page__product-card">
